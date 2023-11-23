27aadcb257f6753dafaabeb061a267ca2c09d1b2
Josue Cano <73917299+Omiced@users.noreply.github.com>
2023-11-23 18:01:42 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,50 +1,27 @@
-// create web
-import React, { Component } from "react";
-import {
-  View,
-  Text,
-  Button,
-  TextInput,
-  StyleSheet,
-  Image,
-  ScrollView,
-  TouchableOpacity,
-  FlatList,
-  Alert,
-} from "react-native";
-import { firebaseApp } from "../components/FirebaseConfig.js";
-import { connect } from "react-redux";
-import { Ionicons } from "@expo/vector-icons";
-import { FontAwesome } from "@expo/vector-icons";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { MaterialIcons } from "@expo/vector-icons";
-import { Feather } from "@expo/vector-icons";
-import { FontAwesome5 } from "@expo/vector-icons";
-import { Entypo } from "@expo/vector-icons";
-import { AntDesign } from "@expo/vector-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import { faComment } from "@fortawesome/free-solid-svg-icons";
-import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
-import { faBookmark } from "@fortawesome/free-solid-svg-icons";
-import { faShareSquare } from "@fortawesome/free-solid-svg-icons";
-import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
-import { faSmile } from "@fortawesome/free-solid-svg-icons";
-import { faCamera } from "@fortawesome/free-solid-svg-icons";
-import { faImage } from "@fortawesome/free-solid-svg-icons";
-import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
-import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
-import { faHeartBroken } from "@fortawesome/free-solid-svg-icons";
-import { faCheckSquare } from "@fortawesome/free-solid-svg-icons";
-import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
-import { faPlusSquare } from "@fortawesome/free-solid-svg-icons";
-import { faSave } from "@fortawesome/free-solid-svg-icons";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-import { faUserFriends } from "@fortawesome/free-solid-svg-icons";
-import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
-import { faUserCheck } from "@fortawesome/free-solid-svg-icons";
-import { faUserTimes } from "@fortawesome/free-solid-svg-icons";
-import { faUserMinus } from "@fortawesome/free-solid-svg-icons";
-import { faUserSlash } from "@fortawesome/free-solid-svg-icons";
-import { faUserAlt } from "@fortawesome/free";
+// Create web server
+
+var express = require("express");
+var router = express.Router();
+var comments = require("../models/comments");
+
+// GET: /comments
+// Return all comments
+router.get("/", function (req, res, next) {
+  comments.selectAll(function (result) {
+    res.json(result);
+  });
+});
+
+// POST: /comments
+// Create new comment
+router.post("/", function (req, res, next) {
+  var data = {
+    name: req.body.name,
+    comment: req.body.comment,
+  };
+  comments.insertComment(data, function (result) {
+    res.json(result);
+  });
+});
+
+module.exports = router;
